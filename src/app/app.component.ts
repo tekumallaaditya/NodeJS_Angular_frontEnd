98f6f42e7ce618514b63aaeac287a58c4ae3159d
Aditya Tekumalla <tekumallaaditya@gmail.com>
2018-07-24 20:45:20 +0200

    del, edit and update done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import{NgForm} from '@angular/forms';
 import {Employees} from './app.model';
 import {EmployeeInfoService} from './app.services';
 
@@ -9,7 +10,11 @@ import {EmployeeInfoService} from './app.services';
 })
 export class AppComponent implements OnInit {
   title = 'Employee Directory App';
-
+  employeeName:string;
+  employeeEmail:string;
+  employeeDOB:string;
+  employeeDepartment:string;
+  employeeGender:string;
   employees:Employees[];
   errorMessage: string;
 
@@ -21,11 +26,29 @@ export class AppComponent implements OnInit {
       console.log('data is->'+ data[0].name );
       this.employees = data,
       (err) => this.errorMessage = err;
-      console.log(this.employees[0].name);
-
-
-      
+      console.log(this.employees[0].name);      
     })
+  }
+
+  editInfo(rowData: any): void{
+    console.log('event works '+ rowData.name);
+    this.employeeName = rowData.name;
+    this.employeeEmail = rowData.email;
+    this.employeeDOB = rowData.DOB;
+    this.employeeDepartment = rowData.department;
+    this.employeeGender = rowData.gender;
+  }
+  delInfo(delData:any):void{
+    console.log('deleting data->'+ delData);
+    this.employeesinfo.DelEmployee(delData).subscribe((response) => {
+      console.log('response is -> ' + response);
+    })    
+  }
+  UpdateInfo(data: NgForm) {
+    console.log('update info is -> '+data.value.Email)
+    this.employeesinfo.UpdateEmployee(data.value).subscribe((response) =>{
+      console.log('response from post request is ->' + response);
+    });
   }
 
 
--- src/app/app.services.ts
+++ src/app/app.services.ts
@@ -18,8 +18,29 @@ export class EmployeeInfoService{
         console.log(this.http.get(this._employeeURL));
         return this.http.get(this._employeeURL).map((response) => response.json()).catch(this.handleError);
     }
+    
+    DelEmployee(delData):Observable<any>{
+        console.log('received del data->'+ delData.email);  
+        const body : any = {email: delData.email}      
+        return this.http.post(this._employeeURL+'del', body).map((response) => response.json()).catch(this.handleError);
+    }
+
+    UpdateEmployee(updateData):Observable<any>{
+        console.log( 'received in services '+ updateData.Name)
+        const body: any = {
+            name : updateData.Name,
+            email : updateData.Email,
+            DOB : updateData.DOB,
+            department: updateData.Department,
+            gender : updateData.Gender,
+            age: updateData.Age            
+        }
+        return this.http.post(this._employeeURL+'update', body).map((response) => response.json()).catch(this.handleError);
+    }
+
     private handleError(error: Response){
-        return Observable.throw(error.json().error || 'server error');
+        console.log(error)
+        return Observable.throw(error.json().error|| 'server error');
     }
     
 }
